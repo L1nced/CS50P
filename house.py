77c2992085bdcb09d1fe9cd0ba761436f450b086
L1nced <l1nced@protonmail.com>
2026-08-07 21:19:22 -0300

    some new .py files

---
+++ house.py
@@ -0,0 +1,9 @@
+name = input("What's your name? ").title().strip()
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
